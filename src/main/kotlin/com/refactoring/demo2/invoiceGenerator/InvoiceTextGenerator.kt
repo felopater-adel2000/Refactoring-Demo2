ebba2cc7ec808c1780f5b5139264a3bda859b1cb
felopater-adel12 <felopater.sadek@maxab.io>
2026-01-16 14:42:15 +0200

    xm

--- src/main/kotlin/com/refactoring/demo2/invoiceGenerator/InvoiceTextGenerator.kt
+++ src/main/kotlin/com/refactoring/demo2/invoiceGenerator/InvoiceTextGenerator.kt
@@ -11,30 +11,7 @@ class InvoiceTextGenerator(val order: Order, val products: Map<String, Product>)
 
             val product = products[item.productID]!!
 
-            var itemCost = 0
-
-            when (product.shippingMethod) {
-                "standard" -> {
-                    itemCost = 500
-                    if (item.weight > 5.0) {
-                        itemCost += (100 * (item.weight - 5.0)).toInt()
-                    }
-                }
-
-                "express" -> {
-                    itemCost = 1200
-                    if (item.weight > 3.0) {
-                        itemCost += (250 * (item.weight - 3.0)).toInt()
-                    }
-
-                    itemCost += 150 * item.quantity
-                }
-
-
-                else -> {
-                    throw kotlin.IllegalArgumentException("unknown shipping method: ${product.shippingMethod}")
-                }
-            }
+            val itemCost = calculateItemCost(product, item)
 
             loyaltyPoints += kotlin.comparisons.maxOf(item.quantity - 2, 0)
             if ("express" == product.shippingMethod) {
@@ -52,6 +29,36 @@ class InvoiceTextGenerator(val order: Order, val products: Map<String, Product>)
         result += "You earned $loyaltyPoints loyalty points\n"
 
         return result
+    }
+
+    private fun calculateItemCost(
+        product: Product,
+        item: ShipmentItem
+    ): Int {
+        var itemCost = 0
+        when (product.shippingMethod) {
+            "standard" -> {
+                itemCost = 500
+                if (item.weight > 5.0) {
+                    itemCost += (100 * (item.weight - 5.0)).toInt()
+                }
+            }
+
+            "express" -> {
+                itemCost = 1200
+                if (item.weight > 3.0) {
+                    itemCost += (250 * (item.weight - 3.0)).toInt()
+                }
+
+                itemCost += 150 * item.quantity
+            }
+
+
+            else -> {
+                throw kotlin.IllegalArgumentException("unknown shipping method: ${product.shippingMethod}")
+            }
+        }
+        return itemCost
     }
 
     fun formatCurrency(amountInCents: Int): String {
